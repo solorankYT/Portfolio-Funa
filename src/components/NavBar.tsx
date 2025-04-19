dcf3345fd3318adc05ded6bf3dce470f64fe9e8a
solorankYT <43119617+solorankYT@users.noreply.github.com>
2025-04-20 03:04:41 +0800

    Installaition

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const NavBar = () => {
+    return (
+        <>
+  <p>Navbar</p>
+
+  <p>Hello</p>
+  </>
+    );
+    }
+
+export default NavBar;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar";
+
+const LandingPage = () => {
+    return (
+
+        
+        <div className="LandingPage">
+            <NavBar />
+        <h1>Welcome to the Landing Page</h1>
+        <p>This is a simple landing page.</p>
+        </div>
+    )
+    }
+export default LandingPage;
